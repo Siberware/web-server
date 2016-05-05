fdff432c468a767a2cbd53fae51a8736534cd16a
Adam Wadsworth <ajw1164@gmail.com>
2016-05-05 07:55:33 -0700

    Updated language

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ var middleware = {
 app.use(middleware.logger);
 
 app.get('/about', middleware.requireAuthentication, function (req, res) {
-	res.send('About us nerds!');
+	res.send('About us nerds!!!');
 });
 
 app.use(express.static(__dirname + '/public'));
